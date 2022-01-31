075f475e02d99f9755dbb99b04accd10815f17b8
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-31 11:23:03 +0100

    05

--- 05/server.js
+++ 05/server.js
@@ -1,13 +1,37 @@
-const logEvents = require('./logEvents')
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fspromises = require('fs').promises;
 
-const eventEmitter = require('events')
 
-class MyEmitter extends eventEmitter {};
+const logEvents = require('./logEvents');
+const eventEmitter = require('events');
+class Emitter extends eventEmitter {};
 
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
 
-myEmitter.on('log', (msg) => logEvents(msg));
 
-setTimeout(()=>{
-    myEmitter.emit('log', 'emitted!!!!')
-}, 2000)
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res)=> {
+    console.log(req.url, req.method)
+
+    let filePath;;
+
+    if (req.url === '/' || req.url === 'index.html') {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/html');
+        
+        fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf8', (err, data) => {
+            res.end(data)
+        });
+    }
+})
+
+server.listen(PORT, ()=> console.log(`server running on ${PORT}`))
+
+
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+// myEmitter.emit('log', 'emitted!!!!')
+
