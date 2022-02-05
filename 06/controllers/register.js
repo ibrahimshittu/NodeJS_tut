27edd94689d7dc31d2df9707790ee913a36d799a
Ibrahim Shittu <ibshittu01@gmail.com>
2022-02-05 15:38:23 +0100

    09

--- 06/controllers/register.js
+++ 06/controllers/register.js
@@ -1,6 +1,6 @@
-const usersBD = {
+const usersDB = {
     users: require('../model/users.json'),
-    setUser: function (data) { this.users == data}
+    setUsers: function (data) { this.users = data}
 }
 
 const fsPromises = require('fs').promises
@@ -12,19 +12,20 @@ const handleNewuser = async (req, res) => {
     if (!username || !password) return res.status(400).json({'message': ' User or Password required'})
 
     // find duplicates
-    const duplicate = usersBD.users.find(person => {
-        person.username == username
+    const duplicate = usersDB.users.find(person => {
+        person.username === username
     })
 
     if (duplicate) return res.status(409).json({'message': 'user already exists'})
     try {
         const hashedPwd = await bcrypt.hash(password, 10)
-        const newUser = {'username': username, 'password': hashedPwd}
-        usersBD.setUser([...usersBD, newUser])
+        const newUser = {"username": username, "password" : hashedPwd}
+        
+        usersDB.setUsers([...usersDB.users, newUser])
 
         await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), 
         JSON.stringify(usersDB.users))
-        console.log(usersBD.users)
+        console.log(usersDB.users)
         res.status(201).json({'message': `user ${username} created`})
     } catch (error) {
         res.json({'message': error.message})
