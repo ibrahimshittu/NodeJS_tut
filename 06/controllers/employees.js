465970470020682a56a4206780e143f2959703c6
Ibrahim Shittu <ibshittu01@gmail.com>
2022-02-07 14:46:47 +0100

    15

--- 06/controllers/employees.js
+++ 06/controllers/employees.js
@@ -48,9 +48,9 @@ const deleteEmployee = async (req, res) => {
 }
 
 const getEmployee = async (req, res) => {
-    if (!req?.body?.id ) return res.status(400).json({'message':'id required'}) 
+    if (!req?.params?.id ) return res.status(400).json({'message':'id required'}) 
 
-    const employee = await Employee.findOne({_id: req.body.id}).exec()
+    const employee = await Employee.findOne({_id: req.params.id}).exec()
     if (!employee ) return res.status(204).json({'message':'no employee'}) 
     res.json(result)
 }
